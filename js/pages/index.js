73c26353fb0fb14d250c35d2d81b16730d8b365a
JustinaSokolova <Justinasal@yandex.ru>
2021-12-27 21:55:27 +0300

    Update

--- js/pages/index.js
+++ js/pages/index.js
@@ -5,12 +5,48 @@ const showTimer = document.querySelector("#ShowTimer");
 const calcBlock = document.querySelector("#CalcBlock");
 const timerBlock = document.querySelector("#TimerBlock");
 
-const hours = document.querySelector("#hours");
-const minutes = document.querySelector("#minutes");
-const seconds = document.querySelector("#seconds");
+const hoursInput = document.querySelector("#hours");
+const minutesInput = document.querySelector("#minutes");
+const secondsInput = document.querySelector("#seconds");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
+
+/////
+
+let secondsValue = 0;
+let minutesValue = 0;
+let hoursValue = 0;
+
+secondsInput.addEventListener("input", (event) => {
+  const value = Number(event.target.value);
+  if (value > 60) {
+    event.target.value = 60;
+  } else if (value < 0) {
+    event.target.value = 0;
+  }
+  secondsValue = value;
+});
+
+minutesInput.addEventListener("input", (event) => {
+  const value = Number(event.target.value);
+  if (value > 60) {
+    event.target.value = 60;
+  } else if (value < 0) {
+    event.target.value = 0;
+  }
+  minutesValue = value;
+});
+
+hoursInput.addEventListener("input", (event) => {
+  const value = Number(event.target.value);
+  if (value < 0) {
+    event.target.value = 0;
+  }
+  hoursValue = value;
+});
+
+/////
 
 showCalc.onclick = OnCalcClick;
 showTimer.onclick = OnTimerClick;
@@ -29,7 +65,14 @@ calcBlock.style.display = "none";
 timerBlock.style.display = "none";
 
 startBtn.onclick = () => {
-  startTimer(timerCallback);
+  if (secondsValue > 0 || minutesValue > 0 || hoursValue > 0) {
+    startTimer(timerCallback, secondsValue, minutesValue, hoursValue);
+    secondsValue = 0;
+    minutesValue = 0;
+    hoursValue = 0;
+  } else {
+    alert("Введите таймер");
+  }
 };
 
 stopBtn.onclick = () => {
@@ -49,7 +92,11 @@ function timerCallback(s, m, h) {
     }
   }
 
-  seconds.innerHTML = format(s);
-  minutes.innerHTML = format(m);
-  hours.innerHTML = format(h);
+  seconds.value = format(s);
+  minutes.value = format(m);
+  hours.value = format(h);
 }
+
+seconds.value = "00";
+minutes.value = "00";
+hours.value = "00";
--- js/utils/timer.js
+++ js/utils/timer.js
@@ -1,22 +1,48 @@
+import "../libs/howler.js";
+
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
 let interval;
 let myCallback;
+let soundActive = false;
 
-function startTimer(callback) {
+function startTimer(callback, initSecond, initMinute, initHour) {
+  seconds = initSecond;
+  minutes = initMinute;
+  hours = initHour;
   myCallback = callback;
   interval = setInterval(() => {
-    seconds++;
-    if (seconds == 60) {
-      minutes++;
-      seconds = 0;
+    seconds--;
+
+    if (!soundActive && seconds <= 10 && hours == 0 && minutes == 0) {
+      var sound = new Howl({
+        src: ["timer.mp3"],
+      });
+      sound.play();
+      soundActive = true;
     }
-    if (minutes == 60) {
-      hours++;
-      minutes = 0;
+
+    //
+    if (seconds < 0) {
+      seconds = 59;
+      if (hours > 0) {
+        if (minutes > 0) {
+          minutes--;
+        } else {
+          minutes = 59;
+          hours--;
+        }
+      } else {
+        minutes--;
+      }
     }
+
+    if (seconds == 0 && minutes == 0 && hours == 0) {
+      resetTimer();
+    }
+
     myCallback(seconds, minutes, hours);
   }, 1000);
 }
@@ -29,6 +55,7 @@ function resetTimer() {
   seconds = 0;
   minutes = 0;
   hours = 0;
+  soundActive = false;
   myCallback(seconds, minutes, hours);
   stopTimer();
 }
